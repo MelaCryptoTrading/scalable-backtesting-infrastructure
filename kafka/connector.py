f3994d5825411146be8d5abcbc708a0d3475930f
nyutu_grace <nyutu_grace15@gmail.com>
2024-06-26 14:32:55 +0100

    connect to kafka cluster, create, fetch and delete topics

--- kafka/connector.py
+++ kafka/connector.py
@@ -1,23 +1,45 @@
-from kafka import KafkaConsumer
-import json
+from kafka.admin.client import KafkaAdminClient
 
-consumer = KafkaConsumer('scenes',
-                         bootstrap_servers='localhost:9092',
-                         auto_offset_reset='earliest',
-                         enable_auto_commit=True,
-                         group_id='backtest-group',
-                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-for message in consumer:
-    scene = json.loads(message.value.decode('utf-8'))
-    # Run backtest with the scene parameters
-    results = run_backtest(scene)  # Define run_backtest function as per your backtesting logic
+class KafkaConnect():
+    def __init__(self):
+        pass
+
+    def connect_to_kafka(self):
+        """
+        Connect to Kafka Cluster on EC2 instance
+        """
+        client = KafkaAdminClient(bootstrap_servers=["b-2.batch6w7.6qsgnf.c19.kafka.us-east-1.amazonaws.com:9092"],
+                                  api_version=(0,10,2))
+        return client        
+        
+    def create_topic(self, topic_list, client):
+        """
+        create topics and add the topics to cluster
+        TODO: test this!!!
+        """
+        client.create_topics(new_topics=topic_list, validate_only=False)
+
+    def fetch_topics_list(self, client):
+        """
+        return a list of topics from Kafka cluster
+        """
+        return client.list_topics()
     
-    # Produce the results back to Kafka
-    produce_results(results)
-
-def produce_results(results):
-    producer = KafkaProducer(bootstrap_servers='localhost:9092',
-                             value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-    producer.send('backtest_results', results)
-    producer.flush()
+    def find_group3_topics(self, topics_list):
+        """
+        return the topics that belong to group 3 only
+        """
+        for topic in topics_list:
+            if topic.startswith('g3'):
+                print(topic)
+        
+    
+    def delete_topic(self,client, topic_name):
+        """
+        delete topic with specific name
+        TODO: test this!!!
+        """
+        client.delete_topics(topic_name)
+# if __name__ == '__main__':
+#     KafkaConnect()
